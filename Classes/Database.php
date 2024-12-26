4b5575af875190199718ddb46d6ec65c023a3574
elmhmmd <youcodemohammed@gmail.com>
2024-12-26 13:47:38 +0100

    creation de la classe de connexion a la base de donnees

--- Classes/Database.php
+++ Classes/Database.php
@@ -1,12 +1,12 @@
 <?php
-// classes/Database.php
+
 class Database {
     private static $instance = null;
     private $pdo;
 
     private function __construct() {
         try {
-            $this->pdo = new PDO("mysql:host=localhost;dbname=africa_geo", "username", "password");
+            $this->pdo = new PDO("mysql:host=localhost;dbname=africa_geo_junior", "root", "");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             throw new Exception("Connection failed: " . $e->getMessage());
